49d576e09ee9d80ade5f558d1245a7874c642db2
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-04-18 15:32:31 +0630

    Create solve class

---
+++ bin/solver.rb
@@ -0,0 +1,19 @@
+class Solver
+  def factorial(num)
+    return 1 if num.zero?
+
+    num * factorial(num - 1)
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 15).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
+end
